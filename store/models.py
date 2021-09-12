8e31869281dc6d438da2f441c4c17aaee5f036a0
Rafael Lima <47rafael.lima@gmail.com>
2021-09-12 01:00:42 -0300

    models update

--- store/models.py
+++ store/models.py
@@ -26,24 +26,50 @@ class Product(Base):
 	price = models.FloatField()
 	is_digital = models.BooleanField(default=False)
 	category = models.ManyToManyField(Category)
-	#images = None
 
 	def __str__(self):
 		return self.title
 
 
 
+class ProductImage(Base):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	#image = None
+
+	def __str__(self):
+		return str(self.id)
+
+
+
+class Order(Base):
+	customer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+	complete = models.BooleanField(default=False)
+
+	def __str__(self):
+		return str(self.id)
+
+
+
+class OrderItem(Base):
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	quantity = models.PositiveIntegerField(default=0)
+
+	def __str__(self):
+		return self.product
+
+
+
 class ShippingInformation(Base):
-	customer = customer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+	customer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
 	address = models.CharField(max_length=300)
 	city =  models.CharField(max_length=300)
 	state =  models.CharField(max_length=300)
 	zipcode = models.CharField(max_length=300)
 
-
-
-class Order(Base):
-	pass
+	def __str__(self):
+		return self.address
 
 
 
@@ -54,7 +80,7 @@ class Review(Base):
 	Review = models.TextField()
 
 	def __str__(self):
-		return "Review - " + self.product
+		return str(self.id)
 
 	class Meta:
-		unique_together = ["user", "product"]
+		unique_together = ["customer", "product"]
